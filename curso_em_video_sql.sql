64fc2ee0a3f02ac1d9fdc7a18e88097ca24b7388
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-08 13:16:24 -0300

    docs: cursoEmVideo create table cursos
    creating table cursos

--- curso_em_video_sql.sql
+++ curso_em_video_sql.sql
@@ -40,3 +40,24 @@ change column profissao prof varchar(20) not null default ''; /* muda o nome da 
 
 alter table pessoas
 rename to gafanhotos;
+
+/*--------------------------------------------------------------------------------------------*/
+/*create table cursos*/
+
+create table if not exists cursos (
+nome varchar(30) not null unique, 
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2016'
+) default charset = utf8;
+
+alter table cursos
+add column idcurso int first;
+
+alter table cursos
+add primary key(idcurso);
+
+desc cursos;
+
+select * from cursos;
